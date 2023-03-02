ad0d50a6dbbdee4694d1d8a3bc63d5fd4fd7cf4f
salom <salom@gmail.com>
2023-03-02 05:39:46 +0500

    men headerni tugatim

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ let xmark = document.getElementById('xmark')
 let evro = document.getElementById('evro')
 let right = document.getElementById('right')
 let leftContent = document.querySelector('.leftContent')
+let down = document.getElementById('down')
+let hedEvo = document.getElementById('evro2')
+let bars = document.getElementById('bars')
 
 xmark.addEventListener('click',(e)=> {
     leftContent.style.display = 'none'
@@ -13,6 +16,15 @@ right.addEventListener('click',(e)=> {
     evro.style.display = 'block'
     setTimeout(()=> {
         evro.style.display = 'none'
-    },2000)
+    },5000)
+})
+down.addEventListener('click',(e)=> {
+    hedEvo.style.display = 'block'
+    setTimeout(()=> {
+        hedEvo.style.display = 'none'
+    },5000)
+})
+bars.addEventListener('click',(e)=> {
+    leftContent.style.display = 'block'
 })
 
